6693b66598344339c17fdb7a8a990a473c70c796
GhavinJ <ghavinj@icloud.com>
2019-09-09 21:09:04 -0400

    Added Private Variables tryMeButton and backgroundView.

--- app/src/main/java/com/ghavinj/tryme/MainActivity.java
+++ app/src/main/java/com/ghavinj/tryme/MainActivity.java
@@ -2,13 +2,32 @@ package com.ghavinj.tryme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private View backgroundView;
+    private Button tryMeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        backgroundView = findViewById(R.id.windowViewId);
+        tryMeButton = (Button)findViewById(R.id.tryMeButton);
+
+        backgroundView.setBackgroundColor(Color.RED);
+
+        tryMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
