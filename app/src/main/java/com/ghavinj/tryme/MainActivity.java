769516528c94dcb0a41975d2f9686e97da8429a4
GhavinJ <ghavinj@icloud.com>
2019-09-09 21:45:57 -0400

    Added valueOf and random Num

--- app/src/main/java/com/ghavinj/tryme/MainActivity.java
+++ app/src/main/java/com/ghavinj/tryme/MainActivity.java
@@ -4,29 +4,41 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
 
     private View backgroundView;
     private Button tryMeButton;
+    private int[] colors;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        colors = new int[]{Color.CYAN, Color.GREEN, Color.RED, Color.BLUE, Color.BLACK, Color.DKGRAY, Color.LTGRAY, Color.MAGENTA, Color.YELLOW};
+
         backgroundView = findViewById(R.id.windowViewId);
         tryMeButton = (Button)findViewById(R.id.tryMeButton);
 
-        backgroundView.setBackgroundColor(Color.RED);
 
         tryMeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+                int colorArrayLength = colors.length;
+                Random random = new Random();
+                int randomNum = random.nextInt(colorArrayLength);
+
+                backgroundView.setBackgroundColor(colors[randomNum]);
+
+                Log.d("Random" , String.valueOf(randomNum));
             }
         });
     }
